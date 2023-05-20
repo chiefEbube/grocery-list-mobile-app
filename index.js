60f7153d0578c68631eb63df4b3b1417212d7d67
chiefEbube <ebubechi.anyanwu@yahoo.com>
2023-05-20 14:46:29 +0100

    baby steps

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-function generateSentence(desc, arr){
-    let str = `The ${arr.length} ${desc} are `
-    for (i = 0; i < arr.length - 1; i++){
-        str += `${arr[i]}, `
+const addBtn = document.querySelector("#add-btn")
+const inputBtn = document.querySelector("#input-field")
+
+addBtn.addEventListener("click", function () {
+    if (inputBtn.value) {
+        console.log(inputBtn.value)
     }
-    str += `${arr[i]}.`
-    return str
-}
+})
 
-const myNames = ["Anyanwu"]
-const describe = "names I have"
-let say = generateSentence(describe, myNames)
 
-console.log(say)
+
+// function generateSentence(desc, arr){
+//     let str = `The ${arr.length} ${desc} are `
+//     for (i = 0; i < arr.length - 1; i++){
+//         str += `${arr[i]}, `
+//     }
+//     str += `${arr[i]}.`
+//     return str
+// }
+
+// const myNames = ["Anyanwu"]
+// const describe = "names I have"
+// let say = generateSentence(describe, myNames)
+
+// console.log(say)
